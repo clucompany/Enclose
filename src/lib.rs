bf0d7459968ec655d3fcdb04989fa51beb1a3cf6
Денис Котляров <denis2005991@gmail.com>
2019-03-03 18:14:48 +0300

    1. Add edition=2019 2. Updating of the license according to 2019, the English version of a name of the developer is added.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//Copyright (c) 2018 #UlinProject Денис Котляров
+//Copyright (c) 2019 #UlinProject Denis Kotlyarov (Денис Котляров)
 
 //Permission is hereby granted, free of charge, to any person obtaining a copy
 //of this software and associated documentation files (the "Software"), to deal
